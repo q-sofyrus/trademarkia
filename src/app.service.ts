d2843ce9a821543441f60353756feae4e78f49c0
q-sofyrus <quasim@sofyrus.com>
2024-10-29 20:39:22 +0530

    adding latest

--- src/app.service.ts
+++ src/app.service.ts
@@ -9,7 +9,7 @@ import { createObjectCsvWriter } from 'csv-writer';
 
 @Injectable()
 export class MyService {
- 
+ private urls=[]
 
   async fetchProxies() {
     const githubUrl = 'https://raw.githubusercontent.com/TheSpeedX/SOCKS-List/master/http.txt';
@@ -23,7 +23,7 @@ export class MyService {
     }
   }
 
-  async fetch( ): Promise<object> {
+  async fetch( ) {
   
     const proxyUrls = await this.fetchProxies();
     const proxyConfiguration = new ProxyConfiguration({ proxyUrls });
@@ -35,10 +35,10 @@ export class MyService {
       // proxyConfiguration,
       persistCookiesPerSession: true,
       maxRequestRetries: 50,
-      maxConcurrency: 10,
+      maxConcurrency: 1,
       minConcurrency: 1,
       navigationTimeoutSecs: 120,
-      requestHandler: async ({ page, request, proxyInfo }) => {
+      requestHandler:async function({ page, request, proxyInfo }) {
         console.log('Scraping:', request.url);
         console.log('Using proxy:', proxyInfo?.url || 'No proxy');
 
@@ -51,35 +51,52 @@ export class MyService {
               })
               .map(anchor => anchor.href);
           });
-
-          for (const link of hrefs) {
-            await this.extractDataFromLink(page, link);
-          }
+          const uniqueHrefs = Array.from(new Set(hrefs));
+            console.log(uniqueHrefs.length)
+            const filePath = 'urlsb.txt';
+            const data = uniqueHrefs.join('\n'); // Join URLs with a newline character
+    
+            fs.appendFile(filePath, data, (err) => {
+                if (err) {
+                    console.error('Error writing to file', err);
+                } else {
+                    console.log('URLs saved successfully to', filePath);
+                }
+            });
+        
+    
+          // for (const link of hrefs) {
+          //   await this.extractDataFromLink(page, link);
+          // }
 
     
           // console.log('Extracted text:', textContent);
           // console.log('formatted data: ',await this.extractValues(textContent))
  
-
+          return hrefs
         } catch (error) {
           console.error(`Failed to navigate to ${request.url}. Error: ${error.message}`);
         }
-      },
+      }.bind(this),
       failedRequestHandler: async ({ request, error }) => {
         console.error(`Request ${request.url} failed too many times. Error: ${error}`);
       },
     });
 
-    const urls = [];
+    
   
    
-
-    console.log('Total links:', urls.length);
-    await crawler.run(['https://www.trademarkia.com/avectas-98141953']);
-    return{'message': 'success'}
+    const urls=[]
+    for (let index = 2; index <=26; index++) {
+      urls.push('https://www.trademarkia.com/category/chemical-products/page-'+String.fromCharCode(index + 96))
+      
+    }
+     console.log('urls:-', urls)
+   await crawler.run(urls);
+    //await this.fetch_data()
   }
  
- 
+
   async saveUrlToCSV(csvData: string, fileName: string): Promise<void> {
     const filePath = path.resolve(__dirname, `${fileName}.csv`);
     fs.appendFile(filePath, csvData + '\n', (err) => {
@@ -112,13 +129,14 @@ export class MyService {
 
   async extractDataFromLink(page:any, url: string) {
     try {
+      console.log("url-",url)
       await page.goto(url, { waitUntil: 'networkidle' });
       const textContent = await page.$eval(
         'div.flex.flex-col.space-y-8.mt-8.ml-8',
         (div) => div.textContent.trim() // Get the text content of the div and trim whitespace
       );
       
-      const extractedValues = this.extractValues(textContent);
+      const extractedValues = await this.extractValues(textContent);
 
       console.log('Extracted values from', url, extractedValues);
     } catch (error) {
